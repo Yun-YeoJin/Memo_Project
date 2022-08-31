1ccba4157fd0d6f5c0650094bf160a1d2d0d5a11
YunYeoJin <duwls0349@naver.com>
2022-08-31 15:25:15 +0900

    Initial Commit

---
+++ Memo_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo_Project
+//
+//  Created by 윤여진 on 2022/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
